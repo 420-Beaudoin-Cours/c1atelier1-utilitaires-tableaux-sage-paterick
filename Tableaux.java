7dbc53915d8a8a78cea643ebb770c769c9ae1d73
sage-paterick <git_paterick@mail.com>
2020-01-24 11:22:30 -0500

    Version1

--- Tableaux.java
+++ Tableaux.java
@@ -1,7 +1,10 @@
+import java.sql.SQLOutput;
+
 /**
  * @author France Beaudoin
- * Modifié!
+ * Modifié par Patrick Provost le 24 janvier 2020
  */
+
 public class Tableaux {
     private static void permute(int[] tab, int i1, int i2){
         int transit = tab[i1];
@@ -39,5 +42,65 @@ public class Tableaux {
             if (imin != i)
                 permute(tab, i, imin);
         }
+    }
+
+    public static void trierDecroissantSSS(int[] tab){
+        int imax;
+        for (int i=0; i<tab.length-1; i++){
+            imax = i;
+            for (int j=i+1; j<tab.length; j++){
+                if (tab[j] > tab[imax])
+                    imax = j;
+            }
+            if (imax != i)
+                permute(tab, i, imax);
+        }
+    }
+
+    public static void print(int[] tab){
+        for (int i: tab){
+            System.out.print(i + " ");
+        }
+    }
+
+    public static int maximum(int[] tab){
+        int imax = 0;
+        for (int i=0; i<tab.length-1; i++){
+            if (tab[i] > tab[imax])
+                imax = i;
+        }
+        return tab[imax];
+    }
+
+    public static int minimum(int[] tab){
+        int imin = 0;
+        for (int i=0; i<tab.length-1; i++){
+            if (tab[i] < tab[imin])
+                imin = i;
+        }
+        return tab[imin];
+    }
+
+    public static double moyenne(int[] tab){
+        int somme = 0;
+        for (int i=0; i<tab.length-1; i++){
+            somme += tab[i];
+        }
+        return somme / tab.length;
+    }
+
+    public static int fouilleSeq(int[] tab, int cherche){
+        boolean trouve = false;
+        int i = 0;
+        while (i<tab.length && !trouve){
+            if (tab[i] == cherche)
+                trouve = true;
+            else
+                i++;
+        }
+        if (trouve)
+            return i;
+        else
+            return -1;
     }
 }
