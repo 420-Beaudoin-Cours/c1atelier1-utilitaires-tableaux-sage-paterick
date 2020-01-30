b7313bfd7e3ff0f4206bd0931102fcbf871f8ac9
sage-paterick <git_paterick@mail.com>
2020-01-30 16:03:00 -0500

    Version2

--- Tableaux.java
+++ Tableaux.java
@@ -2,7 +2,7 @@ import java.sql.SQLOutput;
 
 /**
  * @author France Beaudoin
- * Modifié par Patrick Provost le 24 janvier 2020
+ * Modifié par Patrick Provost le 30 janvier 2020
  */
 
 public class Tableaux {
@@ -83,10 +83,17 @@ public class Tableaux {
 
     public static double moyenne(int[] tab){
         int somme = 0;
+        double moyenne = 0;
+
         for (int i=0; i<tab.length-1; i++){
             somme += tab[i];
         }
-        return somme / tab.length;
+        try {
+            moyenne = somme / tab.length;
+        }
+        catch (ArithmeticException e){
+        }
+        return moyenne;
     }
 
     public static int fouilleSeq(int[] tab, int cherche){
@@ -94,6 +101,73 @@ public class Tableaux {
         int i = 0;
         while (i<tab.length && !trouve){
             if (tab[i] == cherche)
+                trouve = true;
+            else
+                i++;
+        }
+        if (trouve)
+            return i;
+        else
+            return -1;
+    }
+
+    public static void trierDecroissantSSS(String[] tab){
+        int imax;
+        for (int i=0; i<tab.length-1; i++){
+            imax = i;
+            for (int j=i+1; j<tab.length; j++){
+                if (Integer.parseInt(tab[j]) > Integer.parseInt(tab[imax]))
+                    imax = j;
+            }
+            if (imax != i)
+                permute(tab, i, imax);
+        }
+    }
+
+    public static void print(String[] tab){
+        for (String s: tab) {
+            System.out.print(s + " ");
+        }
+    }
+
+    public static int maximum(String[] tab){
+        int imax = 0;
+        for (int i=0; i<tab.length-1; i++){
+            if (Integer.parseInt(tab[i]) > Integer.parseInt(tab[imax]))
+                imax = i;
+        }
+        return Integer.parseInt(tab[imax]);
+    }
+
+    public static int minimum(String[] tab){
+        int imin = 0;
+        for (int i=0; i<tab.length-1; i++){
+            if (Integer.parseInt(tab[i]) < Integer.parseInt(tab[imin]))
+                imin = i;
+        }
+        return Integer.parseInt(tab[imin]);
+    }
+
+    public static double moyenne(String[] tab){
+        int somme = 0;
+        double moyenne = 0;
+
+        for (int i=0; i<tab.length-1; i++){
+            somme += Integer.parseInt(tab[i]);
+        }
+        try {
+            moyenne = somme / tab.length;
+        }
+        catch (ArithmeticException e){
+        }
+        return moyenne;
+    }
+
+    public static int fouilleSeq(String[] tab, int cherche){
+        boolean trouve = false;
+        int i = 0;
+        while (i<tab.length && !trouve){
+            if (Integer.parseInt(tab[i]) == cherche)
                 trouve = true;
             else
                 i++;
